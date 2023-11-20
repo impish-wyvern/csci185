8f049f8a728b6b831a8011fd11511dd6f5e75a36
Emily Kerr <emkerr15@gmail.com>
2023-11-20 13:15:50 -0500

    project

--- Kards10/Kards10/inc/sendEmail.php
+++ Kards10/Kards10/inc/sendEmail.php
@@ -1,4 +1,4 @@
-﻿<?php
+﻿<!-- <?php
 
 // Replace this with your own email address
 $siteOwnersEmail = 'emkerr15@gmail.com';
@@ -65,5 +65,30 @@ if($_POST) {
 	} # end if - there was a validation error
 
 }
+
+?> -->
+
+<?php
+
+    $to = "jtdenton14@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message.";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
+    $fields{"message"} = "message";
+
+
+
+    $body = "Here is what was sent:\r\n"; 
+
+    foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
+
+
+    $send = mail($to, $subject, $body, $headers);
 
 ?>
