0e074507a43ca8d63bbdbde4a70a0da68b948992
Emily Kerr <emkerr15@gmail.com>
2023-11-29 17:03:31 -0500

    project

--- Kards10/Kards10/js/main.js
+++ Kards10/Kards10/js/main.js
@@ -227,7 +227,9 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+			  url: "https://formsubmit.co/emkerr15@gmail.com",
+		    //   url: "inc/sendEmail.php",
+		
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
